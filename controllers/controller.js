7ea8ce401ea59bce76cbe3b5b8100dce548c2945
Sophia <sophia.oquias@gmail.com>
2024-05-27 15:51:19 +0800

    fixed formatting

--- controllers/controller.js
+++ controllers/controller.js
@@ -5,21 +5,21 @@ const path = require('path');
 const { ObjectId } = require('mongodb');
 
 exports.getAllEntries = function (req, res) {
-    postModel.getAllEntries({ user: req.session.user }, function (entries) {
-        userModel.getById(req.session.user, function(err, result) {
-            res.render("index", { 
-                entry: entries,
-                budgetGoal: result.budgetGoal,
-                savingsGoal: result.savingsGoal
-            });
-        });
-    });
+	postModel.getAllEntries({ user: req.session.user }, function (entries) {
+		userModel.getById(req.session.user, function(err, result) {
+			res.render("index", { 
+				entry: entries,
+				budgetGoal: result.budgetGoal,
+				savingsGoal: result.savingsGoal
+			});
+		});
+	});
 }
 
 exports.login = function (req, res) {
-    res.render("login", { layout: "login-layout" });
+	res.render("login", { layout: "login-layout" });
 }
 
 exports.signup = function (req, res) {
-    res.render("signup", { layout: "login-layout" });
+	res.render("signup", { layout: "login-layout" });
 }
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,120 +1,149 @@
-const { check } = require("express-validator");
+const {
+	check
+} = require("express-validator");
 const userModel = require("../database/models/User");
 const postModel = require("../database/models/Post");
-const {validationResult} = require('express-validator');
-const {ObjectId} = require('mongodb');
-const bcrypt = require('bcrypt');
+const {
+	validationResult
+} = require("express-validator");
+const {
+	ObjectId
+} = require("mongodb");
+const bcrypt = require("bcrypt");
 
 exports.registerUser = (req, res) => {
-  const errors = validationResult(req);
+	const errors = validationResult(req);
 
-  console.log(errors);
+	console.log(errors);
 
-  if (errors.isEmpty()) {
-    const {username, email, password} = req.body;
+	if (errors.isEmpty()) {
+		const {
+			username,
+			email,
+			password
+		} = req.body;
 
-    userModel.getOne({email: email}, (err, result) => {
-      if (result) {
-        console.log(result);
-        //if email already exists: throw error
-        req.flash('error_msg', 'User already exists. Please login');
-        res.redirect('/login');
-      } else {
-        //create user
-        const saltRounds = 10;
-        bcrypt.hash(password, saltRounds, (err, hashed) => {
-          const newUser = {
-            username,
-            email,
-            password: hashed
-          };
+		userModel.getOne({
+				email: email,
+			},
+			(err, result) => {
+				if (result) {
+					console.log(result);
+					//if email already exists: throw error
+					req.flash("error_msg", "User already exists. Please login");
+					res.redirect("/login");
+				} else {
+					//create user
+					const saltRounds = 10;
+					bcrypt.hash(password, saltRounds, (err, hashed) => {
+						const newUser = {
+							username,
+							email,
+							password: hashed,
+						};
 
-          userModel.create(newUser, (err, user) => {
-            if (err) {
-              console.log(err);
-              req.flash('error_msg', 'Could not create user. Please try again');
-              res.redirect('/signup');
-            }
-            else {
-              req.flash('success_msg', 'You are now registered! Please login');
-              res.redirect('/login');
-            }
-          });
-        });
-      }
-    })
-  } else {
-    const messages = errors.array().map((item) => item.msg);
+						userModel.create(newUser, (err, user) => {
+							if (err) {
+								console.log(err);
+								req.flash(
+									"error_msg",
+									"Could not create user. Please try again"
+								);
+								res.redirect("/signup");
+							} else {
+								req.flash(
+									"success_msg",
+									"You are now registered! Please login"
+								);
+								res.redirect("/login");
+							}
+						});
+					});
+				}
+			}
+		);
+	} else {
+		const messages = errors.array().map((item) => item.msg);
 
-    req.flash('error_msg', messages.join(' '));
-    res.redirect('/signup');
-  }
-}
+		req.flash("error_msg", messages.join(" "));
+		res.redirect("/signup");
+	}
+};
 
 exports.loginUser = (req, res) => {
-  const errors = validationResult(req);
+	const errors = validationResult(req);
 
-  if (errors.isEmpty()) {
-    const {username, password} = req.body;
+	if (errors.isEmpty()) {
+		const {
+			username,
+			password
+		} = req.body;
 
-    // Next items go here... Same as before, this will be replaced.
-    userModel.getOne({ username: username }, (err, User) => {
-      if (err) {
-        // Database error occurred...
-        req.flash('error_msg', 'Something happened! Please try again.');
-        res.redirect('/login');
-      } else {
-        // Successful query
-        if (User) {
-          // User found!
-    
-          bcrypt.compare(password, User.password, (err, result) => {
-            //passwords match (result == true)
-            if (result) {
-              //update session object once matched
-              req.session.user = User._id;
-              req.session.username = User.username;
-              req.session.email = User.email; 
+		// Next items go here... Same as before, this will be replaced.
+		userModel.getOne({
+				username: username,
+			},
+			(err, User) => {
+				if (err) {
+					// Database error occurred...
+					req.flash("error_msg", "Something happened! Please try again.");
+					res.redirect("/login");
+				} else {
+					// Successful query
+					if (User) {
+						// User found!
 
-              console.log(req.session);
+						bcrypt.compare(password, User.password, (err, result) => {
+							//passwords match (result == true)
+							if (result) {
+								//update session object once matched
+								req.session.user = User._id;
+								req.session.username = User.username;
+								req.session.email = User.email;
 
-              res.redirect('/');
-            }
-            else { //passwords don't match
-              req.flash('error_msg', 'Incorrect password. Please try again');
-              res.redirect('/login');
-            }
-          });
-        } else {
-          // No user found
-          req.flash('error_msg', 'No registered user with that username. Please register.');
-          res.redirect('/signup');
-        }
-      }
-    });
-  } else {
-      const messages = errors.array().map((item) => item.msg);
+								console.log(req.session);
 
-      req.flash('error_msg', messages.join(' '));
-      res.redirect('/login');
-  }
+								res.redirect("/");
+							} else {
+								//passwords don't match
+								req.flash("error_msg", "Incorrect password. Please try again");
+								res.redirect("/login");
+							}
+						});
+					} else {
+						// No user found
+						req.flash(
+							"error_msg",
+							"No registered user with that username. Please register."
+						);
+						res.redirect("/signup");
+					}
+				}
+			}
+		);
+	} else {
+		const messages = errors.array().map((item) => item.msg);
+
+		req.flash("error_msg", messages.join(" "));
+		res.redirect("/login");
+	}
 };
 
 exports.logoutUser = (req, res) => {
-  if (req.session) {
-    req.session.destroy(() => {
-      res.clearCookie('connect.sid');
-      res.redirect('/login');
-    });
-  }
+	if (req.session) {
+		req.session.destroy(() => {
+			res.clearCookie("connect.sid");
+			res.redirect("/login");
+		});
+	}
 };
 
 exports.viewAccount = function (req, res) {
-  userModel.getById(req.session.user, function(err, data) {
-    res.render("view-account", {
-      layout: "no-new-entry",
-      email: data.email,
-      username: data.username
-    });
-  });
-}
+	userModel.getById(req.session.user, function (err, data) {
+		res.render("view-account", {
+			layout: "no-new-entry",
+			email: data.email,
+			username: data.username,
+		});
+	});
+};
